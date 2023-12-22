d2e57cbabe735b0bb58a4faffeaf4a0b5317026b
christhetree <christhetree@gmail.com>
2023-12-22 15:56:33 -0400

    [cm] Adding conv tests for same padding

---
+++ neutone_sdk/conv.py
@@ -0,0 +1,181 @@
+import logging
+import os
+from typing import Optional, Tuple, Union
+
+import torch as tr
+import torch.nn.functional as F
+from torch import Tensor
+from torch import nn
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(level=os.environ.get('LOGLEVEL', 'INFO'))
+
+
+class PaddingCached(nn.Module):
+    def __init__(self,
+                 n_ch: int,
+                 padding: int,
+                 use_dynamic_bs: bool = True,
+                 batch_size: int = 1,
+                 debug_mode: bool = True) -> None:
+        """
+        Cached padding for cached convolutions. Handles dynamic batch sizes by default
+        at the expense of dynamic memory allocations.
+
+        Args:
+            n_ch: Number of channels.
+            padding: Number of padding samples.
+            use_dynamic_bs: If True, the padding will dynamically change batch size to
+                            match the input.
+            batch_size: If known, the initial batch size can be specified here to avoid
+                        dynamic changes.
+            debug_mode: If True, assert statements are enabled.
+        """
+        super().__init__()
+        if debug_mode:
+            assert n_ch > 0
+            assert batch_size > 0
+        self.n_ch = n_ch
+        self.padding = padding
+        self.use_dynamic_bs = use_dynamic_bs
+        self.debug_mode = debug_mode
+        self.register_buffer("pad_buf", tr.zeros((batch_size, n_ch, padding)))
+
+    def reset(self, batch_size: Optional[int] = None) -> None:
+        if batch_size is not None:
+            self.pad_buf = self.pad_buf.new_zeros((batch_size, self.n_ch, self.padding))
+        else:
+            self.pad_buf.zero_()
+
+    def prepare_for_inference(self) -> None:
+        self.debug_mode = False
+        self.reset()
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.debug_mode:
+            assert x.ndim == 3  # (batch_size, in_ch, samples)
+        # We support padding == 0 for convolutions with kernel size of 1
+        if self.padding == 0:
+            return x
+
+        bs = x.size(0)
+        if self.use_dynamic_bs and bs != self.pad_buf.size(0):
+            self.reset(bs)
+        elif self.debug_mode:
+            assert bs == self.pad_buf.size(0)
+
+        x = tr.cat([self.pad_buf, x], dim=-1)  # Concat input to the cache
+        self.pad_buf = x[..., -self.padding:]  # Discard old cache
+        return x
+
+
+class Conv1dGeneral(nn.Module):
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int,
+                 stride: int = 1,
+                 padding: Union[str, int, Tuple[int], Tuple[int, int]] = "same",
+                 dilation: int = 1,
+                 bias: bool = True,
+                 padding_mode: str = "zeros",
+                 causal: bool = True,
+                 cached: bool = False,
+                 use_dynamic_bs: bool = True,
+                 batch_size: int = 1,
+                 debug_mode: bool = True) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.padding_mode = padding_mode
+        self.causal = causal
+        self.cached = cached
+        self.debug_mode = debug_mode
+
+        left_padding, right_padding = self._calc_padding(kernel_size,
+                                                         stride,
+                                                         padding,
+                                                         dilation,
+                                                         causal)
+        self.left_padding = left_padding
+        self.right_padding = right_padding
+        self.uncached_padding = (left_padding, right_padding)
+
+        self.conv1d = nn.Conv1d(in_channels,
+                                out_channels,
+                                kernel_size=(kernel_size,),
+                                stride=(stride,),
+                                padding=0,
+                                dilation=(dilation,),
+                                bias=bias,
+                                padding_mode=padding_mode)
+        if padding_mode == "zeros":
+            # After setting the torch conv padding mode, we need to change it to be
+            # compatible with F.pad(). It's strange that this is inconsistent in torch.
+            self.padding_mode = "constant"
+        self.padding_cached = PaddingCached(in_channels,
+                                            left_padding,
+                                            use_dynamic_bs,
+                                            batch_size,
+                                            debug_mode)
+
+    def _calc_padding(self,
+                      kernel_size: int,
+                      stride: int,
+                      padding: Union[str, int, Tuple[int], Tuple[int, int]],
+                      dilation: int,
+                      causal: bool) -> Tuple[int, int]:
+        if isinstance(padding, tuple) and len(padding) == 1:
+            padding = padding[0]
+
+        if padding == "valid":
+            return 0, 0
+        elif padding == "same":
+            assert stride == 1, "If padding is 'same', stride must be 1"
+            pad_amount = (kernel_size - 1) * dilation
+            if causal:
+                return pad_amount, 0
+            elif pad_amount % 2 == 0:
+                return pad_amount // 2, pad_amount // 2
+            else:
+                # Favor right padding over left padding just like in nn.Conv1d
+                return pad_amount // 2, pad_amount // 2 + 1
+        elif isinstance(padding, int):
+            assert padding >= 0
+            if causal:
+                return padding, 0
+            else:
+                return padding, padding
+        else:
+            assert len(padding) == 2, "Expected padding to be a tuple of length 2."
+            assert padding[0] >= 0 and padding[1] >= 0
+            if causal:
+                assert padding[1] == 0, "If causal, right padding must be 0"
+            return padding
+
+    def set_cached(self, cached: bool) -> None:
+        self.cached = cached
+        self.reset()
+
+    def reset(self, batch_size: Optional[int] = None) -> None:
+        self.padding_cached.reset(batch_size)
+
+    def prepare_for_inference(self) -> None:
+        self.debug_mode = False
+        self.conv1d.eval()
+        self.padding_cached.prepare_for_inference()
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.debug_mode:
+            assert x.ndim == 3  # (batch_size, in_ch, samples)
+            assert x.size(1) == self.in_channels
+        if self.cached:
+            x = self.padding_cached(x)
+            if self.right_padding > 0:
+                # TODO(cm): prevent dynamic memory allocations here
+                x = F.pad(x, (0, self.right_padding), mode=self.padding_mode)
+        elif self.uncached_padding != (0, 0):
+            # TODO(cm): prevent dynamic memory allocations here
+            x = F.pad(x, self.uncached_padding, mode=self.padding_mode)
+        x = self.conv1d(x)
+        return x
---
+++ testing/test_conv.py
@@ -0,0 +1,90 @@
+import itertools
+import logging
+import os
+import random
+from typing import Union, Tuple
+
+import torch as tr
+from torch import nn
+from tqdm import tqdm
+
+from neutone_sdk.conv import Conv1dGeneral
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
+
+
+def _test_against_conv_torch(in_channels: int,
+                             out_channels: int,
+                             kernel_size: int,
+                             padding: Union[str, int, Tuple[int]],
+                             dilation: int,
+                             causal: bool,
+                             padding_mode: str = "zeros",
+                             batch_size: int = 1,
+                             block_size: int = 128,
+                             n_blocks: int = 64) -> None:
+    conv_gen = Conv1dGeneral(in_channels,
+                             out_channels,
+                             kernel_size,
+                             padding=padding,
+                             dilation=dilation,
+                             padding_mode=padding_mode,
+                             causal=causal,
+                             cached=False)
+    padding_torch = padding
+    if causal:
+        assert conv_gen.right_padding == 0
+        padding_torch = 0
+    conv_torch = nn.Conv1d(in_channels,
+                           out_channels,
+                           kernel_size,
+                           padding=padding_torch,
+                           dilation=dilation,
+                           padding_mode=padding_mode)
+
+    conv_torch.weight = nn.Parameter(conv_gen.conv1d.weight.clone())
+    conv_torch.bias = nn.Parameter(conv_gen.conv1d.bias.clone())
+
+    audio = tr.rand((batch_size, in_channels, n_blocks * block_size))
+    out_torch = conv_torch(audio)
+    out_gen = conv_gen(audio)
+    if causal:
+        out_gen = out_gen[..., conv_gen.left_padding:]
+    assert out_gen.shape == out_torch.shape
+    assert tr.allclose(out_gen, out_torch)
+
+    if conv_gen.right_padding == 0:
+        # TODO(cm): support cached right padding
+        conv_gen.set_cached(True)
+        out_blocks = []
+        for idx in range(n_blocks):
+            audio_block = audio[..., idx * block_size:(idx + 1) * block_size]
+            out_blocks.append(conv_gen(audio_block))
+        out_cached = tr.cat(out_blocks, dim=-1)
+        out_cached = out_cached[..., conv_gen.left_padding:]
+        assert out_cached.shape == out_torch.shape
+        assert tr.allclose(out_cached, out_torch)
+
+
+def test_conv1d_general():
+    # causal_flags = [False, True]
+    causal_flags = [True]
+    in_channels = [1, 2]
+    out_ch = 1
+    # kernel_sizes = [1, 2, 3, 4, 5]
+    kernel_sizes = [2, 3, 4, 5]
+    dilations = [1, 2, 3, 4, 8]
+
+    for causal, in_ch, kernel_size, dil in tqdm(itertools.product(causal_flags,
+                                                                  in_channels,
+                                                                  kernel_sizes,
+                                                                  dilations)):
+        log.info(f"Testing causal={causal}, in_ch={in_ch}, kernel_size={kernel_size}, dil={dil}")
+        # _test_against_conv_torch(in_ch, out_ch, kernel_size, padding="same", dilation=dil, causal=causal)
+        _test_against_conv_torch(in_ch, out_ch, kernel_size, padding="valid", dilation=dil, causal=causal)
+
+
+if __name__ == "__main__":
+    test_conv1d_general()

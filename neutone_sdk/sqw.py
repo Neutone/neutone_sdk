7c4387c90f96937d2423f1b69b7581fa24b9a6b6
Christopher Mitcheltree <christhetree@gmail.com>
2024-02-01 19:37:08 +0800

    [cm] Adding offline rendering method (#42)
    * [cm] Adding offline rendering method
    
    * [cm] Adding comment
    
    * [cm] Removing no grad decorators
    
    * [cm] Removing TODO

--- neutone_sdk/sqw.py
+++ neutone_sdk/sqw.py
@@ -251,7 +251,7 @@ class SampleQueueWrapper(nn.Module):
                 validate_waveform(model_out, self.is_output_mono())
             self.out_queue.push(model_out)
 
-    @tr.no_grad()
+    @tr.jit.export
     def forward(self, x: Tensor, params: Optional[Tensor] = None) -> Tensor:
         is_daw_mono = x.size(0) == 1
         in_n = x.shape[1]
@@ -273,7 +273,6 @@ class SampleQueueWrapper(nn.Module):
         return x
 
     @tr.jit.export
-    @tr.no_grad()
     def forward_bt(
         self, x: Tensor, params: Optional[Tensor] = None
     ) -> Optional[Tensor]:
@@ -299,6 +298,43 @@ class SampleQueueWrapper(nn.Module):
         if curr_n == 0:
             return None
         return self.bt_out_buffer[0:daw_n_ch, 0:curr_n]
+
+    @tr.jit.export
+    def forward_offline(self, x: Tensor, params: Optional[Tensor] = None) -> Tensor:
+        self.reset()
+
+        delay_samples = self.calc_buffering_delay_samples() + self.calc_model_delay_samples()
+        if self.use_debug_mode:
+            assert x.ndim == 2
+            if params is not None:
+                assert params.ndim == 2
+                assert x.size(1) == params.size(1)
+
+        n_samples = x.size(1)
+        # Ensure we pad enough to make up for any delay
+        padding_amount = tr.ceil(tr.tensor((n_samples + delay_samples) / self.daw_bs)) * self.daw_bs - n_samples
+        padding_amount = int(padding_amount.item())
+        padded_audio = tr.nn.functional.pad(x, [0, padding_amount])
+        audio_chunks = padded_audio.split(self.daw_bs, dim=1)
+
+        param_chunks = []
+        if params is not None:
+            padded_params = tr.nn.functional.pad(params, [0, padding_amount], mode="replicate")
+            param_chunks = padded_params.split(self.daw_bs, dim=1)
+
+        # TODO(cm): if memory is an issue, we can preallocate everything beforehand
+        out_chunks = []
+        for idx, audio_chunk in enumerate(audio_chunks):
+            if params is None:
+                out = self.forward(audio_chunk, params=None).clone()
+            else:
+                param_chunk = param_chunks[idx]
+                out = self.forward(audio_chunk, params=param_chunk).clone()
+            out_chunks.append(out)
+
+        audio_out = tr.cat(out_chunks, dim=1)
+        audio_out = audio_out[:, -n_samples:]
+        return audio_out
 
     @tr.jit.export
     def is_input_mono(self) -> bool:

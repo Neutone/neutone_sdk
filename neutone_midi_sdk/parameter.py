3ea82891653c216dcc9cb602f46edf325d8f6923
caenopy <nickpbecker@gmail.com>
2024-03-10 21:17:30 -0700

    handle numerical/tensor params separately

--- neutone_midi_sdk/parameter.py
+++ neutone_midi_sdk/parameter.py
@@ -1,48 +1,9 @@
-##################
-# OLD
-##################
-
-# from enum import Enum
-# from typing import NamedTuple, Dict
-
-
-# class NeutoneParameterType(Enum):
-#     KNOB = "knob"
-# class NeutoneParameter(NamedTuple):
-#     """
-#     Define a Neutone Parameter that the user can use to control the model.
-
-#     The name and the description of the parameter will be shown as a tooltip
-#     within the UI. `default_value` must be between 0 and 1 and will be used
-#     as a default in the plugin when no presets are available.
-#     """
-
-#     name: str
-#     description: str
-#     default_value: float
-#     type: NeutoneParameterType = NeutoneParameterType.KNOB
-#     used: bool = True
-
-#     def to_metadata_dict(self) -> Dict[str, str]:
-#         return {
-#             "name": self.name,
-#             "description": self.description,
-#             "type": self.type.value,
-#             "used": str(self.used),
-#             "default_value": str(self.default_value),
-#         }
-
-
-##################
-# NEW
-##################
-
 import logging
 import os
 from abc import ABC
 from enum import Enum
-import torch
-from typing import Dict, Optional, Set, List, Union
+import torch as tr
+from typing import Dict, Union, Tuple
 
 from neutone_midi_sdk import constants
 
@@ -68,7 +29,7 @@ class NeutoneParameter(ABC):
         self,
         name: str,
         description: str,
-        default_value: Union[int, float, str], #TODO(nic): optional default_value for tensor case, or default to uniformly populating tensor with default_value 
+        default_value: Union[int, float, str, tr.Tensor], #TODO(nic): optional default_value for tensor case, or default to uniformly populating tensor with default_value 
         used: bool,
         param_type: NeutoneParameterType,
     ):
@@ -85,7 +46,7 @@ class NeutoneParameter(ABC):
             "description": self.description,
             "default_value": str(self.default_value),
             "used": str(self.used),
-            "type": self.type.value,
+            "type": str(self.type.value),
         }
 
 class ContinuousNeutoneParameter(NeutoneParameter):
@@ -116,73 +77,20 @@ class TensorNeutoneParameter(NeutoneParameter):
     """
     Defines a tensor Neutone Parameter that the user can use to control a model.
     """
-    def __init__(self,
-                 name: str,
-                 description: str,
-                 shape: List[int],
-                 default_tensor: Optional[torch.Tensor] = None,
-                 used: bool = True):
-        super().__init__(name, description, used)
+    def __init__(self, name: str, description: str, shape: Tuple[int], default_value: tr.Tensor, used: bool = True):
+        super().__init__(
+            name,
+            description, 
+            default_value,
+            used,
+            NeutoneParameterType.TENSOR,
+            )
         self.shape = shape
-        self.default_tensor = default_tensor
-        self.type = NeutoneParameterType.TENSOR
 
     def to_metadata_dict(self) -> Dict[str, str]:
         """Returns a string dictionary containing the metadata of the parameter."""
         data = super().to_metadata_dict()
         data["shape"] = str(self.shape)
-        data["default_tensor"] = str(self.default_tensor.numpy())
+        data["default_value"] = str(self.default_value.numpy())
+        data["tokenize"] = "True"
         return data
-
-
-
-
-
-
-
-# from enum import Enum
-# from typing import Dict
-
-
-# class NeutoneParameterType(Enum):
-#     KNOB = "knob"
-#     AUX_INPUT_TENSOR = "aux_input_tensor"
-
-# class NeutoneParameter:
-#     """
-#     Define a Neutone Parameter that the user can use to control the model.
-
-#     The name and the description of the parameter will be shown as a tooltip
-#     within the UI. `default_value` must be between 0 and 1 and will be used
-#     as a default in the plugin when no presets are available.
-#     """
-
-#     def __init__(
-#             self,
-#             name: str,
-#             description: str,
-#             dim: int = 1,
-#             default_value: list = [0.0], # TODO: adjust type here
-#             used: bool = True
-#         ):
-    
-
-#         self.name = name
-#         self.description = description
-#         self.dim = dim
-#         self.default_value = default_value
-#         self.used = used
-
-#         if self.dim == 1:
-#             self.type = NeutoneParameterType.KNOB
-#         else:
-#             self.type = NeutoneParameterType.AUX_INPUT_TENSOR
-        
-#     def to_metadata_dict(self) -> Dict[str, str]:
-#         return {
-#             "name": self.name,
-#             "description": self.description,
-#             "type": self.type.value,
-#             "used": str(self.used),
-#             "default_value": str(self.default_value),
-#         }

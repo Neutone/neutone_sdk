818a78b713b864fcdb3ac66edfb8f20bcf97ae0c
caenopy <nickpbecker@gmail.com>
2024-03-10 21:17:30 -0700

    handle numerical/tensor params separately

--- neutone_midi_sdk/core.py
+++ neutone_midi_sdk/core.py
@@ -15,40 +15,42 @@ class NeutoneMIDIModel(tr.nn.Module):
                  tokenizer_data: TokenData):
 
         super().__init__()
-        self.MAX_N_PARAMS = constants.MAX_N_PARAMS
+        self.MAX_N_NUMERICAL_PARAMS = constants.MAX_N_NUMERICAL_PARAMS
+        self.MAX_N_TENSOR_PARAMS = constants.MAX_N_TENSOR_PARAMS
         self.SDK_VERSION = constants.SDK_VERSION
-
         self.n_neutone_parameters = len(self.get_neutone_parameters())
 
+        # Allocate default numerical params to prevent dynamic allocations later
+        numerical_default_param_vals = self._get_numerical_default_param_values()
+        assert len(numerical_default_param_vals) <= self.MAX_N_NUMERICAL_PARAMS, (
+            f"Number of default numerical parameter values ({len(numerical_default_param_vals)}) "
+            f"exceeds the maximum allowed ({self.MAX_N_NUMERICAL_PARAMS})."
+        )
+        numerical_default_param_values_t = tr.tensor([v for _, v in numerical_default_param_vals])
         # Ensure number of parameters is within the maximum allowed
-        assert self.n_neutone_parameters <= self.MAX_N_PARAMS
+        self.n_numerical_neutone_parameters = len(numerical_default_param_vals)
+        assert self.n_numerical_neutone_parameters <= self.MAX_N_NUMERICAL_PARAMS
         # Ensure parameter names are unique
         assert len(set([p.name for p in self.get_neutone_parameters()])) == len(
             self.get_neutone_parameters()
         )
+        self.register_buffer("tensor_default_param_values", numerical_default_param_values_t.unsqueeze(-1))
+
+        # Allocate default tensor params to prevent dynamic allocations later
+        tensor_default_param_vals = self._get_tensor_default_param_values()
+        assert len(tensor_default_param_vals) <= self.MAX_N_TENSOR_PARAMS, (
+            f"Number of default tensor parameter values ({len(numerical_default_param_vals)}) "
+            f"exceeds the maximum allowed ({self.MAX_N_TENSOR_PARAMS})."
+        )
+        # TODO(nic): this assumes a common dimension for all tensor parameters
+        tensor_default_param_values_t = tr.cat([v for _, v in tensor_default_param_vals])
+        self.register_buffer("numerical_default_param_values", tensor_default_param_values_t.unsqueeze(-1))
 
         # Save parameter metadata
         self.neutone_parameters_metadata = {
             f"p{idx + 1}": p.to_metadata_dict()
             for idx, p in enumerate(self.get_neutone_parameters())
         }
-
-        # Allocate default params buffer to prevent dynamic allocations later
-        numerical_default_param_vals = self._get_numerical_default_param_values()
-        tensor_default_param_vals = self._get_tensor_default_param_values()
-
-        default_param_values_t = tr.tensor([v for _, v in numerical_default_param_vals])
-        default_param_values_t.extend([v for _, v in tensor_default_param_vals])
-
-        assert len(numerical_default_param_vals) <= self.MAX_N_PARAMS, (
-            f"Number of default parameter values ({len(numerical_default_param_vals)}) "
-            f"exceeds the maximum allowed ({self.MAX_N_PARAMS})."
-        )
-        # TODO(nic): we may need a check on the number of tensor params here depending
-        # on how we handle them in the plugin
-
-        default_param_values = default_param_values_t.unsqueeze(-1)
-        self.register_buffer("default_param_values", default_param_values)
 
         # Allocate remapped params dictionary to prevent dynamic allocations later
         self.remapped_params = {
@@ -61,6 +63,7 @@ class NeutoneMIDIModel(tr.nn.Module):
                 for name, val in tensor_default_param_vals
             }
         )
+        self.default_param_values = self.remapped_params
 
         # Save parameter information
         self.neutone_parameter_names = [p.name for p in self.get_neutone_parameters()]
@@ -72,36 +75,6 @@ class NeutoneMIDIModel(tr.nn.Module):
         self.neutone_parameter_types = [
             p.type.value for p in self.get_neutone_parameters()
         ]
-
-        ####################################
-        # Old constructor
-        # TODO(nic): remove when migrating to main Neutone core infra
-
-        # parameters
-        # neutone_parameters = self.get_neutone_parameters() # now handled by _get_numerical_default_param_values()
-        # if len(neutone_parameters) < self.MAX_N_PARAMS:    # now handled in MidiToMidiBase 
-        #     neutone_parameters += [
-        #                               NeutoneParameter(
-        #                                   name="",
-        #                                   description="",
-        #                                   used=False,
-        #                                   default_value=0.0,
-        #                               )
-        #                            ] * (self.MAX_N_PARAMS - len(neutone_parameters))
-
-        # default_param_values = tr.tensor(               # now handled in default_param_values_t
-        #     [
-        #         neutone_parameter.default_value
-        #         for neutone_parameter in neutone_parameters
-        #     ]
-        # ).unsqueeze(-1)
-        # self.register_buffer("default_param_values", default_param_values)
-        # self.remapped_params = {
-        #     neutone_param.name: default_param_values[idx]
-        #     for idx, neutone_param in enumerate(self.get_neutone_parameters())
-        # }
-
-        ####################################
 
         # instantiate model
         model.eval()
@@ -185,7 +158,7 @@ class NeutoneMIDIModel(tr.nn.Module):
         return self.neutone_parameters_metadata
 
     @tr.jit.export
-    def get_default_param_values(self) -> Tensor:
+    def get_default_param_values(self) -> Dict[str, Tensor]:
         """
         Returns the default parameter values as a tensor of shape
         (N_DEFAULT_PARAM_VALUES, 1).
